c944ee3be6d76bd0679460d63eb4005cdaf9637a
Syed M. Taha <s.m.taha10@gmail.com>
2020-03-19 10:23:38 +0500

    fix(env.config): rename file

--- env.config.js
+++ env.config.js
@@ -1,19 +1,19 @@
-const { lowerCase } = require("lodash");
+const { lowerCase } = require('lodash');
 
 function getCurrentEnv() {
   let currentEnv = lowerCase(process.env.NODE_ENV);
   if (!currentEnv) {
-    currentEnv = "";
+    currentEnv = '';
   }
   return currentEnv;
 }
 
-const isDevEnv = getCurrentEnv() === "dev";
+const isDevEnv = getCurrentEnv() === 'dev';
 
 let env;
 if (isDevEnv) {
   // tslint:disable-next-line: no-var-requires
-  const { config } = require("dotenv");
+  const { config } = require('dotenv');
   const { parsed } = config();
   env = parsed;
 } else {
